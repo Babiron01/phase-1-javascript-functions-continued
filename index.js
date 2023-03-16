76fa76c0e951c1911c44c43e55bc2d604d5e208d
Babikip01 <126768788+Babikip01@users.noreply.github.com>
2023-03-16 13:18:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(doSomething="roller-skate") {
+    return `This Saturday, I want to ${doSomething}!`
+    }
+    saturdayFun()
+
+    function mondayWork(someWhere="go to the office") {
+        return `This Monday, I will ${someWhere}.`
+        }
+        mondayWork()
+        
+        function wrapAdjective (style="*") {
+            return function(stringOne="special"){
+              return `You are ${style}${stringOne}${style}!`
+            }
+        }     
